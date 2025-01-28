5824f2a3051b7c0aba343640b49d4d1f06d2e5a6
Artem Krylov <kav128@outlook.com>
2025-01-28 22:59:25 +0300

    init

---
+++ Texnokaktus.ProgOlymp.OpenTelemetry/DiExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace Texnokaktus.ProgOlymp.OpenTelemetry;
+
+public static class DiExtensions
+{
+    public static IServiceCollection AddOpenTelemetry(this IServiceCollection services,
+                                                      IConfiguration configuration,
+                                                      Action<TracerProviderBuilder>? tracerProviderConfigurationAction,
+                                                      Action<MeterProviderBuilder>? meterProviderConfigurationAction)
+    {
+        services.AddOpenTelemetry()
+                .ConfigureResource(builder => builder.AddService(Process.GetCurrentProcess().ProcessName))
+                .WithTracing(tracerProviderBuilder =>
+                 {
+                     tracerProviderBuilder.AddAspNetCoreInstrumentation()
+                                          .AddHttpClientInstrumentation()
+                                          .AddRedisInstrumentation()
+                                          .AddSqlClientInstrumentation()
+                                          .AddGrpcClientInstrumentation()
+                                          .AddOtlpExporter(options =>
+                                           {
+                                               options.Endpoint = new(configuration.GetConnectionString("OtlpReceiver")!);
+                                               options.Protocol = OtlpExportProtocol.Grpc;
+                                           });
+
+                     tracerProviderConfigurationAction?.Invoke(tracerProviderBuilder);
+                 })
+                .WithMetrics(meterProviderBuilder =>
+                 {
+                     meterProviderBuilder.AddAspNetCoreInstrumentation()
+                                         .AddSqlClientInstrumentation()
+                                         .AddHttpClientInstrumentation()
+                                         .AddPrometheusExporter();
+
+                     meterProviderConfigurationAction?.Invoke(meterProviderBuilder);
+                 });
+
+        return services;
+    }
+}
